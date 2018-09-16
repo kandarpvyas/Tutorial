cd9f00367d9cdb1ba3a179641bba93fdb94a74a0
kandarpvyas <kandarpvyas.23@gmail.com>
2018-09-15 23:03:03 -0400

    Project Files

---
+++ src/main/java/LeetCode/InsertInterval.java
@@ -0,0 +1,55 @@
+package LeetCode;
+
+import java.util.LinkedList;
+
+public class InsertInterval {
+    public static void main(String args[]) {
+        InsertInterval obj = new InsertInterval();
+
+        LinkedList<Interval> list = new LinkedList<Interval>();
+
+        list.add(new Interval(1,3));
+        list.add(new Interval(6,9));
+
+        LinkedList<Interval> result = new LinkedList<Interval>();
+
+
+        result = obj.insert(list,new Interval(2,5));
+
+        for(Interval rec : result)
+        {
+            System.out.println(rec.getStart()+"-----:::"+rec.getEnd());
+        }
+    }
+
+    public LinkedList<Interval> insert(LinkedList<Interval> intervals, Interval newInterval) {
+
+        if (newInterval == null)
+            return intervals;
+
+        LinkedList<Interval> list = new LinkedList<Interval>();
+
+        if (intervals == null || intervals.size() == 0) {
+            list.add(newInterval);
+            return list;
+        }
+
+        for (Interval cur : intervals) {
+            // insert cur
+            if (newInterval.getStart() > cur.getEnd()) {
+                list.add(cur);
+                // insert interval
+            } else if (newInterval.getEnd() < cur.getStart()) {
+                list.add(newInterval);
+                newInterval = cur;
+                // merge interval
+            } else {
+                newInterval.setStart(Math.min(newInterval.getStart(), cur.getStart()));
+                newInterval.setEnd(Math.max(newInterval.getEnd(), cur.getEnd()));
+            }
+        }
+        list.add(newInterval);
+
+        return list;
+    }
+}
---
+++ src/main/java/LeetCode/SubSets.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+public class SubSets {
+    public static void main(String args[]) {
+
+    }
+
+    public void Solution(int[] num) {
+        
+    }
+}
